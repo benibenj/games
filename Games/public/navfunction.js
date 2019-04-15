4dd0742f2c64d0fa4fdb860c54415d6a35d624f5
Benjamin <Benjamin@10.5.183.160>
2019-04-15 23:14:33 +0200

    Login Form

--- Games/public/navfunction.js
+++ Games/public/navfunction.js
@@ -1,3 +1,5 @@
+// Nav Controll
+
 var open = true;
 function controllNav(){
 	var nav = document.getElementById('nav');
@@ -17,4 +19,52 @@ function closeNav(){
 		nav.style.transform = "translate(-100%)";
 	}
 	open = false;
+}
+
+
+// Forms
+
+let signup = false;
+function opensignup(){
+	if (signup) {
+		document.getElementById("signup").style.display = "none";
+		document.getElementById("clicker").style.display = "none";
+		signup = false;
+	}
+	else{
+		document.getElementById("signup").style.display = "block";
+		signup = true;
+		document.getElementById("clicker").style.display = "block";
+		document.getElementById("login").style.display = "none";
+		login = false;
+	}
+	
+}
+
+let login = false;
+function openlogin(){
+	if (login) {
+		document.getElementById("login").style.display = "none";
+		document.getElementById("clicker").style.display = "none";
+		login = false;
+	}
+	else{
+		document.getElementById("login").style.display = "block";
+		login = true;
+		document.getElementById("clicker").style.display = "block";
+		document.getElementById("signup").style.display = "none";
+		signup = false;
+	}
+	
+}
+
+
+//By Clicking the Background Canvas, closing the form
+
+function closeform(){
+	document.getElementById("clicker").style.display = "none";
+	document.getElementById("signup").style.display = "none";
+	document.getElementById("login").style.display = "none";
+	signup = false;
+	login = false;
 }
