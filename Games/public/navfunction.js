4548d418d9714c596c40f70a2db7b6885bf50900
Benjamin <Benjamin@10.5.183.160>
2019-04-16 13:57:30 +0200

    Login

--- Games/public/navfunction.js
+++ Games/public/navfunction.js
@@ -27,16 +27,12 @@ function closeNav(){
 let signup = false;
 function opensignup(){
 	if (signup) {
-		document.getElementById("signup").style.display = "none";
 		document.getElementById("clicker").style.display = "none";
-		signup = false;
+		closeAll("none");
 	}
 	else{
-		document.getElementById("signup").style.display = "block";
-		signup = true;
+		closeAll("signup");
 		document.getElementById("clicker").style.display = "block";
-		document.getElementById("login").style.display = "none";
-		login = false;
 	}
 	
 }
@@ -44,27 +40,80 @@ function opensignup(){
 let login = false;
 function openlogin(){
 	if (login) {
-		document.getElementById("login").style.display = "none";
 		document.getElementById("clicker").style.display = "none";
-		login = false;
+		closeAll("none");
 	}
 	else{
-		document.getElementById("login").style.display = "block";
-		login = true;
+		closeAll("login");
 		document.getElementById("clicker").style.display = "block";
-		document.getElementById("signup").style.display = "none";
-		signup = false;
 	}
 	
 }
+
+
+// Opens Profile
+let profile = false;
+function openprofile(){
+	if (profile) {
+		document.getElementById("clicker").style.display = "none";
+		closeAll("none");
+	}
+	else{
+		closeAll("profile");
+		document.getElementById("clicker").style.display = "block";
+	}
+}
+
 
 
 //By Clicking the Background Canvas, closing the form
 
 function closeform(){
 	document.getElementById("clicker").style.display = "none";
-	document.getElementById("signup").style.display = "none";
+	closeAll("none");
+}
+
+
+
+function closeAll(id){
 	document.getElementById("login").style.display = "none";
-	signup = false;
+	document.getElementById("signup").style.display = "none";
+	document.getElementById("profile").style.display = "none";
+	document.getElementById("uid").style.display = "none";
+	document.getElementById("mail").style.display = "none";
+	document.getElementById("pwd").style.display = "none";
+	document.getElementById("overview").style.display = "block";
+	profile = false;
 	login = false;
+	signup = false;
+
+	document.getElementById(id).style.display = "block";
+
+	switch(id) {
+  		case "profile": profile = true;
+  		break;
+  		case "login": login = true;
+  		break;
+  		case "signup": signup = true;
+  		break;
+  		default:;
+	}
+}
+
+
+/* Settings */
+
+function openuid(){
+	document.getElementById("uid").style.display = "block";
+	document.getElementById("overview").style.display = "none";
+}
+
+function openmail(){
+	document.getElementById("mail").style.display = "block";
+	document.getElementById("overview").style.display = "none";
+}
+
+function openpwd(){
+	document.getElementById("pwd").style.display = "block";
+	document.getElementById("overview").style.display = "none";
 }
