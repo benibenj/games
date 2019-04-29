9ae1bc33144cb3c01412aa4fa90eef8a89762da8
fabianboesiger <boesiger.fabian@outlook.com>
2019-04-29 22:03:38 +0200

    Changes

--- Games/src/Main.java
+++ Games/src/Main.java
@@ -1,15 +1,29 @@
 import java.io.File;
 import java.io.IOException;
 
+import database.Database;
+import mailer.Mailer;
+import manager.DatabaseSessionManager;
 import responder.RenderResponder;
 import server.Request;
 import server.Server;
+import user.User;
+import user.UserManager;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		RenderResponder responder = new RenderResponder(new File("views"));
-		Server server = new Server(8000, new File("public"), responder);
+		Database database = new Database();
+		Mailer mailer = new Mailer(new File("views/mail"));
+		RenderResponder responder = new RenderResponder(new File("views/web"));
+		DatabaseSessionManager <User> sessionManager = new DatabaseSessionManager <User> (database, 7 * 24 * 60 * 60, User::new);
+		Server server = new Server(8000, new File("public"), responder, sessionManager);
 		
+		initializeRoutes(server, responder);
+		
+		new UserManager(server, responder, database, mailer);
+	}
+	
+	private static void initializeRoutes(Server server, RenderResponder responder) {
 		server.on("GET", "/", (Request request) -> {
 			return responder.render("index.html");
 		});
