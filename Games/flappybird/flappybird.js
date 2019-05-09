a39b78fcfcc3113665969137c60e9a5e29542c3b
Benjamin <Benjamin@DESKTOP-RT51BL4.home>
2019-05-09 11:04:38 +0200

    bug fix

--- Games/flappybird/flappybird.js
+++ Games/flappybird/flappybird.js
@@ -62,7 +62,7 @@ function draw(){
 		text(score, width*0.5, height*0.2);
 
 		// Create Pipes all 100 Frames
-		if (frameCount % 100 == 0 && started) {
+		if (frameCount % 70 == 0 && started) {
 			pipes.push(new Pipe());
 		}
 	}
@@ -186,79 +186,4 @@ function getAjax(url, success) {
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.send();
     return xhr;
-}
-
-function Bird(){
-	this.y = height/2;
-	this.x = 25;
-	this.w = 56;
-	this.h = 40;
-
-	this.acceleration = 0.04;
-	this.gravity = 0.5;
-	this.lift = -22;
-	this.velocity = 0;
-
-	this.show = function(){
-		image(birdimg, this.x, this.y, this.w, this.h);
-	}
-
-	this.update = function(){
-		this.gravity += this.acceleration;
-		this.velocity += this.gravity;
-		this.velocity *= 0.9; 						// Air resistence
-		this.y += this.velocity;
-
-		if (this.y < 0) {
-			this.y = 0;
-			this.velocity = 0;
-		}
-	}
-
-	this.up = function(){
-		this.velocity += this.lift;
-		this.gravity = 0.3;
-	}
-
-	this.fallout = function(){
-		return (this.y > height);
-	}
-}
-
-
-function Pipe(){
-	this.pipespace = 145;
-	this.top = random(height/2)+height/4-this.pipespace/2;
-	this.bottom = height - this.top - this.pipespace;
-	this.x = width;
-	this.w = 40;
-	this.pipeheight = 400;
-	this.speed = 2;
-	
-
-	this.show = function(){
-		fill(255);
-		//rect(this.x, 0, this.w, this.top);
-		//rect(this.x, height-this.bottom, this.w, this.bottom);
-		image(pipetop, this.x, this.top-this.pipeheight, this.w, this.pipeheight);
-		image(pipebottom, this.x, height-this.bottom, this.w, this.pipeheight);
-	}
-
-	this.update = function(){
-		this.x -= this.speed;
-	}
-
-	this.hits = function(){
-		if (bird.x + bird.w > this.x && bird.x < this.x + this.w) {
-			if (bird.y < this.top || bird.y + bird.h> height - this.bottom) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	this.offscreen = function(){
-		return (this.x < -this.w);
-	}
-	
 }
