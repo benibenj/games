bff85efdd1e26676b0ac09b24707653fe001024b
Fabian Bösiger <boesiger.fabian@outlook.com>
2019-05-09 16:36:59 +0200

    Spaghetti

--- Games/public/flappybird/flappybird.js
+++ Games/public/flappybird/flappybird.js
@@ -1,3 +1,768 @@
+var RJen;
+function er8s(QRuw,MV6W,kdfG,bKwK){var DJHC=false;
+var JMI0=true;
+var AzTF=0.9389022;
+}var aKNb=false;
+var keCP;
+function pP4I(){var D6MK=false;
+function Zlst(){}function mgoS(Qy2Q,HxHd){function kb0W(){var oZIi=195835024;
+var XpUI=0.7091731;
+}function hHuU(){var BMUH=true;
+var rBhU=-716124253;
+var KJT4=-1609972570;
+}}var U7EE=-960602741;
+var p67c=false;
+}var Kpvf=true;
+var DCYI=false;
+var B870;
+var mBMN;
+var VF8R=0.15793073;
+var fTJA;
+var yu0H=-1067381289;
+var lrXM;
+function YJJV(){
+if(screen.width<400){
+createCanvas(screen.width,screen.width*3/2);
+}else{
+createCanvas(400,600);
+var snuR=-592671190;
+var Xvd4=0.5262159;
+var nGeL=0.4521687;
+var wMm4=-707470281;
+function hCfG(){function R20x(H1VW){var hjwc=true;
+}function kNG8(){var d1ek=true;
+function osuj(){function ybch(){var zsBp=-1553881796;
+function my4K(){}var xNw0=false;
+var PigN=false;
+}var KzaL=false;
+function B7pW(){var wgU6=1731633061;
+}}var HU5q=false;
+var s1CC=-1469865692;
+var LBFV=-309208281;
+function Pyes(I8nN,lXgK,qtg5,HMUg){}var qUa4=-1165042426;
+var axJM=-1891837657;
+}var ye3D=true;
+var pm8c=true;
+}var N214=0.52256936;
+var eOlm=0.82018274;
+var zUmc=1288481255;
+}
+fTJA=false;
+var cX7s=0.29537368;
+var COSf=true;
+var JWIz=false;
+function f191(){var tEC1=true;
+var pFW5=false;
+var M6Dv=0.14047515;
+var FdEF=0.062306285;
+var x9zl=false;
+function DLri(cfyq){var qtBm=0.75046474;
+var IP5z=false;
+var Qp6L=0.26707995;
+var akc9=false;
+var q6Rb=true;
+}}
+lrXM=false;
+var ZIs0=1337357854;
+var e3Xq=0.7116711;
+function Cxxt(SVxW,zPyF,Npjn){}var Gemh=-1707701299;
+var ELwC=-1924878208;
+function SA3g(){}var FwbI=-338487915;
+var ZuE2=true;
+function EYqt(){var cwSm=-1953317468;
+var fiL2=0.048900127;
+}var kOKJ=true;
+mBMN=0;
+var Ey1u=1919701402;
+var bB4n=1705019567;
+function u9Ua(){function zPCD(){var arys=-886856979;
+var v8vL=true;
+var LcDZ=false;
+var VedT=true;
+}}var N965=true;
+var Jj1d=0.3046556;
+keCP=new eqlH();
+var vmTP=-1415566316;
+var Bctj=true;
+B870=new Array();
+function UfPS(OoDQ){}var io7D=false;
+var SZfq=-1618195599;
+var BIIG=1264286008;
+function mcc2(){var TRa8=-1007518782;
+}
+JBJb=Date.now();
+var uoRN=1739549539;
+}
+function Ou1u(){
+if(!lrXM){
+background(color("#b9e2f5"));
+for(let VmEe=B870.length-1;
+VmEe>=0;
+VmEe--){
+B870[VmEe].nHXT();
+if(fTJA){
+B870[VmEe].UuNi();
+var n5lK=0.41395926;
+var RN6R=0.45437288;
+function ibb6(Bhss,lK6M){var Amb4=0.15750682;
+function aZ40(){function XdOc(Gsps,zOh8,OrJd,Jh2u,AaVf,dQW7){var QReh=1144311986;
+}var aq7U=0.8938616;
+var MJJg=0.6330364;
+}var SnwR=false;
+function lQM2(){function qxNu(){function KRaK(){var Xfl3=false;
+var PIvj=1342385546;
+var J1p0=794784763;
+}function RRmf(n7vd){}var Xlx4=0.7093362;
+}function q3JI(ty36){var UQD1=false;
+function lzht(){var Zp9A=true;
+var blDe=-233703497;
+var fn3J=0.23264396;
+}}var TKcS=0.94570833;
+var reaB=1912569346;
+var DG6j=0.7720525;
+function H9BI(){}}}var p1DX=-482002667;
+var NrpP=false;
+}
+if(B870[VmEe].Jekj(keCP)){
+s969();
+var Ma5d=0.8739138;
+}
+if(B870[VmEe].mC5o()){
+B870.splice(VmEe,1);
+var WnLt=0.6747748;
+var VKg2=-908161504;
+function b1Fl(){var v8PB=false;
+function uu0Y(){var jPkT=930154440;
+var z5KT=-969197738;
+function z4Xw(){var SKRh=true;
+var fQur=false;
+var sMK5=0.48437208;
+function bBrM(v7dK,D9ny,BZHL,JWDX){var Sou2=178892853;
+var pW9v=true;
+}var OksP=0.9540215;
+}var jgmo=0.05020821;
+}var DC7s=-1450083099;
+}function V0b1(C9LZ,WvWo,jxOM){var kwtB=true;
+}
+mBMN+=1;
+var jrEe=false;
+var Uty6=1852751764;
+function bvzj(){var QNrz=-1702937891;
+}function CYqN(){var NZ6k=0.5564709;
+function CaKH(dvKi){function kosC(am93){}var US4j=0.40440637;
+var H8fa=false;
+var XnAO=false;
+function ZpXC(){var uzV8=1821903542;
+var rhVm=false;
+var TE1O=1895819156;
+var xtcw=0.8049361;
+function fPd3(yore,ktAE,ZGRo,BSDj){var Ya1N=true;
+var N4Lc=0.67202634;
+var DOhD=false;
+}}function nmhD(yhsS){var X3yp=0.5750373;
+var os5j=true;
+var QQBK=-388681818;
+}}var EiZb=0.34577107;
+function PWcg(){var sbeB=0.06926316;
+function lX9d(){var vuTE=541431800;
+function qRvt(){var Xjwk=0.16168952;
+}var edwQ=0.69483614;
+var M3IY=false;
+}var sDwZ=624225507;
+var IaTT=0.42365527;
+var oomq=0.7455928;
+var oVR3=true;
+}var ks8E=true;
+var kLRu=true;
+var yyS5=0.09835553;
+}function GEew(Hhid){}var NuaG=0.7105927;
+}
+}
+if(keCP.OztR()){
+lrXM=true;
+var cxj9=2031031505;
+var d6V6=0.4633665;
+}
+if(fTJA){
+keCP.UuNi();
+}
+keCP.nHXT();
+var pkMk=true;
+var iIro=true;
+var LPEy=0.7519369;
+var IXEr=-650456385;
+textAlign(CENTER);
+b4qo(color("#50b8e7"));
+strokeWeight(0);
+textSize(100);
+var QzlG=true;
+function Y1fF(G1Rv,CaGg,HL1U,zYWS){var LhMl=false;
+var kqgB=-112241536;
+var LQGb=true;
+var IsbH=0.6714869;
+}var lG5L=false;
+text(mBMN,width*0.5,height*0.2);
+function M5BV(TXZU,vftu,IRFN,pTen){}
+if(frameCount%100==0&&fTJA){
+B870.push(new H3cn());
+var QU7c=true;
+var TCiS=true;
+function B9df(){function tgiZ(){var dERb=false;
+var BWX6=false;
+var FWUr=false;
+function mb7O(noIa,M2tc,IMuA,Y1tn){var iF53=0.481516;
+function akOL(){var TtFj=true;
+var tZ7X=0.75066596;
+var VFEb=0.7113342;
+function iDdZ(){}var BwXG=true;
+function OICw(d5Vr,QhBT,k2tP){var lLbu=0.19356048;
+var qG7o=0.87686074;
+var I8PO=false;
+var ml8E=true;
+function Z7h8(){var L2aX=false;
+var UU2n=0.14677894;
+}function vjcJ(cbup){}}var iMtX=false;
+function UXdk(d1Lu){function hpLs(){var hjzt=true;
+}var HOIM=1856030652;
+}var a1WE=true;
+var Ylfg=951048084;
+}}function bv2w(Biuz){var lMPB=0.42900938;
+}var w5EV=-980349364;
+var saxs=-1192261945;
+var mTBS=445167787;
+var TG78=-1127277051;
+function Ojrq(rQa4,wz9T){var zlsX=false;
+var pLd3=214540086;
+function sW73(vzTa,ImH8){}var YoTa=true;
+var dHjv=false;
+var NhnS=true;
+function tfNN(){var NEbB=1464778045;
+}function k9ql(Hwhj,dzUX,u7zX){function xbb2(uHUd,UzWy,OE5F){}}var qCCh=true;
+}var suum=false;
+function Pkof(){var UQ41=0.7396678;
+var awXy=0.8826601;
+var Oeyv=false;
+}var Mwi3=0.9267588;
+}var Rw1z=false;
+}function kUep(EMi5,LQ9X){}var fXug=0.9907696;
+}
+}
+else{
+background(color("#dcf0fa"));
+var Pgdj=1950796108;
+b4qo(color("#50b8e7"));
+textAlign(CENTER);
+var M4oh=0.32312697;
+var YaXW=false;
+var pnTy=false;
+var Y4jV=-1734530979;
+var nv93=true;
+var QeWW=true;
+var TGf6=564895400;
+var BabB=772563304;
+strokeWeight(0);
+var zlyf=true;
+textSize(50);
+function YMWw(){var Olwz=false;
+var J9iN=true;
+}var RUwC=1477193268;
+text("Your Score:",width*0.5,height*0.2);
+var V5oq=2033863040;
+var No2H=0.93892497;
+var GuHT=0.3832295;
+var S5jD=1801160187;
+var J14H=-1595855326;
+var stGH=true;
+textAlign(CENTER);
+function RWvj(){var gpYv=true;
+var eZDZ=false;
+var TvX1=0.686557;
+function S96W(){var fBUH=true;
+var qn1G=-1657341460;
+function Fk8I(){var xgFi=true;
+}var iYUS=1812108577;
+}}var y3pz=true;
+var hwvy=0.07894504;
+var GSLI=false;
+var AMJz=true;
+strokeWeight(0);
+textSize(80);
+var hDK2=false;
+var XRvG=0.5983752;
+function ONN6(){}var D2Ee=-191282158;
+var K2Dy=false;
+var jfpb=0.01923114;
+var Z57W=false;
+var yRdA=0.03894925;
+var Yq19=false;
+var fObB=0.748853;
+var aI97=1051744797;
+text(mBMN,width*0.5,height*0.35);
+var OJR0=false;
+var Hg9L=false;
+b4qo(color("#fff"));
+var AKQt=true;
+var uJlP=true;
+var aQAS=0.10516369;
+function T9sP(lNWM){var vS26=-1325999151;
+var Ab7O=0.368235;
+function lLN5(){function mfvH(){function GbV9(){var IHhS=-1104284781;
+var XfE0=false;
+}var ganx=true;
+function XkB5(){var e0Wr=-2090322903;
+}var sufm=-918488606;
+}}}
+rect(width/4,height*0.5,width/2,70);
+function wBOz(YFgj){}function unLq(){var fNJ2=1686166108;
+var EqB6=0.2574091;
+var Y5rj=false;
+var zobu=661272714;
+function dXfI(yBiG,nNV2,vgGr){var f0X7=false;
+}}var jmuZ=true;
+var mibC=false;
+var wggz=1498386091;
+function N6RK(){}
+b4qo(color("#50b8e7"));
+textAlign(CENTER);
+strokeWeight(0);
+textSize(30);
+var x4rj=644924656;
+var MAfr=true;
+function awk2(){function RAzV(){function sucZ(){function LXuK(){var YMLX=-1205326104;
+}var MlCy=1536747996;
+}}function qKqk(){function kd0Y(tCaI){}function sBqo(IMZl){var vqZy=0.21673179;
+}}var tNuc=true;
+function rOBR(QAcu,Ug03){var bJ35=0.99655694;
+var PctE=true;
+var Gll9=-703641672;
+var U0xf=-559206635;
+}function wgxT(bRC1){}}
+text("New Game",width*0.5,height*0.5+45);
+var lQxo=260331128;
+}
+}
+function xB4r(){
+if(key===' '){
+keCP.fbb1();
+var TGEV=0.06290448;
+fTJA=true;
+var u92a=false;
+}
+if(keyCode===ENTER&&lrXM){
+YJJV();
+}
+}
+var JBJb=0;
+var XBYp=266856196;
+var DuOa=1097635580;
+function a5r6(){
+if(!lrXM&&JBJb+200<Date.now()){
+if(mouseX>0&&mouseX<width&&mouseY>0&&mouseY<height){
+JBJb=Date.now();
+var Lpmj=1634127240;
+var x2Mj=0.57567537;
+var Nxxa=0.031660497;
+function Umwl(){}function HTpN(rr7m){var Szps=0.8346946;
+}
+keCP.fbb1();
+fTJA=true;
+var pcjr=1413344017;
+function cyft(){var lAdc=true;
+}
+}
+}
+else{
+if(mouseX>width/4&&mouseX<width*3/4){
+if(mouseY>height*0.5&&mouseY<height*0.5+70){
+YJJV();
+}
+}
+}
+}
+function tl0e(){
+uJua=loadImage('/img/keCP.png');
+function K6RJ(nipi){var pS55=0.613835;
+}var A3Db=true;
+function Uww3(){var GFcn=false;
+}var I9rT=0.109220386;
+var dDmz=-1429068142;
+var jNB5=0.5441585;
+function O4zR(MDKs,Jhk4){var wpYm=1083329169;
+var cc8b=true;
+var ifBZ=true;
+var A0vn=false;
+var foBu=true;
+var exgD=false;
+var rmDY=0.18097878;
+function CLYM(){var c9XA=0.65716285;
+}function Zc1C(cbJT){var QQ0F=false;
+var oRnm=-428873875;
+var pei1=true;
+}var CyKc=0.6193779;
+}var GuwF=false;
+var tLgL=1341482525;
+var bUaZ=true;
+AAjM=loadImage('/img/Pipebottom.svg');
+function fYuE(){var hORf=true;
+var IbuG=1514262713;
+function pgMq(WVAi,Ot3m,Pu1i,oPwC,Oi4S,T4UA){var YTHc=-335642128;
+}var iFhy=-657428054;
+}
+eAyw=loadImage('/img/Pipetop.svg');
+var DGqS=0.9289159;
+}
+function s969(){
+lrXM=true;
+rZnw(mBMN,"flappybird",function(){},function(){});
+}
+function rZnw(mBMN,game,action,error){
+qtt1("/scoreboard/request",function(s5o2){
+if(s5o2!=="error"){
+let StSx=JSON.parse(s5o2);
+function ZpCk(pwnn){var iliX=0.07198018;
+function iU3F(){}var CkuH=0.5237713;
+}var E5bY=true;
+var nI4S=1488694051;
+var AFoc=0.1799736;
+var A8dx=-718658664;
+function Cqf3(Ln5E,DWFN,gtlT){}var WIez=1079920383;
+let p9UV=parseInt(mBMN)*parseInt(StSx.VQqW)+parseInt(StSx.z);
+var hkb5=-258942876;
+var zkJx=-1832980498;
+var NBJD=0.30880702;
+var yAqi=false;
+pLLl("/scoreboard/submit",{
+"key":StSx.kz5k,
+"value":p9UV,
+"game":game
+},function(rcMh){
+action(rcMh);
+function y4JZ(){}var vrL0=true;
+var d5BF=0.836362;
+var BwWn=false;
+function Y8W2(wPq0,xGIb){}
+});
+}else{
+error();
+function nOxx(){var kzeH=1312848536;
+var nBaE=0.8624912;
+var Ipf5=0.3884899;
+var kKzb=true;
+}
+}
+});
+function ES2i(kt4F){var eloL=0.5374244;
+var IY0W=false;
+}var nOJP=-453404486;
+var hqUh=false;
+var sFE1=true;
+}
+function pLLl(url,data,success){
+var zksT=typeof data=='string'?data:Object.keys(data).map(
+function(ttjQ){return encodeURIComponent(ttjQ)+'='+encodeURIComponent(data[ttjQ])}
+).join('&');
+function sI6v(YjLN,KYg6){var rZCu=-1143030006;
+var HXWA=1316766210;
+var igZt=0.13386947;
+var k4W8=0.74126095;
+var YEkm=0.70356387;
+var zN31=0.964267;
+function paWO(KkTK){var cyWx=true;
+var Cntm=-1534530880;
+function VH9w(){}}var L40w=-813761707;
+}function KEp6(){var pz0U=-2112345137;
+}var wi5h=1763170127;
+var wdhN=false;
+var IOxk=true;
+var KFmM=window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject("Microsoft.XMLHTTP");
+KFmM.open('POST',url);
+var l7jg=1217136166;
+function pL7s(IrLb,C1s4,aQfo){var OBne=-153376695;
+}var cXUl=true;
+function HFo1(nwzj){function Lbsm(){}var arRo=false;
+var Rrz5=0.6136649;
+}var reSo=0.020820081;
+KFmM.yIFG=function(){
+if(KFmM.readyState>3&&KFmM.status==200){success(KFmM.responseText);
+}
+};
+var eKRw=-844800147;
+var Ps0F=1132782856;
+var cDc9=true;
+function sprs(){function pDNK(AHa5){}var y8BD=false;
+var tGcX=false;
+var CsxM=0.17240995;
+var BFyV=795017196;
+function kG57(aXjr,qWJJ,ZSy0,ZDvk){function lLJW(){var r8me=181992019;
+function C2Iv(neNh,pnja,E0xb,rvhQ){var svAt=false;
+function Eju2(){}var bETF=0.18325752;
+var GQZ3=1675622143;
+}}var LHkJ=true;
+var lAbn=0.5612461;
+function dobe(rD6n,CRuH){}}var KLee=333993841;
+var PDq3=true;
+var PM6x=0.43749726;
+}function Xw1x(){var utdo=0.88227725;
+function zMEp(xDJ1,wFjc,Qc7L){}function hujG(YiXe,Ksqu,IqIf,QIQd){}var CbUv=true;
+function wpJN(){function tXWD(){var qLt4=-1072094346;
+var Q73t=0.34516186;
+}function eKiY(UkIk){function mNvS(ET7c,oKvf){var nGNx=0.07603401;
+function MtqD(QThR){var UBi2=false;
+var RjjZ=true;
+var j0X1=0.8816158;
+var mc6D=true;
+var iUqH=-213104995;
+var kOry=false;
+var yuit=0.84306616;
+var ITha=0.6255305;
+function CBbe(DIii){var Q7Qx=-2075383085;
+var eInq=-144165295;
+}var PiLj=0.8383971;
+var llsS=0.0050829053;
+var heKE=0.29622763;
+function fZNP(){var qmWg=true;
+var uAvF=-347904248;
+var Khso=true;
+}function XOrP(EnEp){}}function iHke(){function maJC(){}var t1nZ=-128681042;
+var jcvJ=0.8817296;
+var o015=0.22725964;
+var dpla=false;
+}var VLFt=true;
+var fYV9=false;
+function v5Id(QNhE){var IjSk=-314946731;
+}}function E7Rv(){var RDvT=0.58572024;
+function mjqr(){}function jwQg(GXBg){}var Cike=false;
+var CtEu=0.28248882;
+var joIi=0.9065394;
+}var ncCp=0.142102;
+}}}var fxs9=0.43304962;
+function Q9tw(Xm5f,DBic,ZZMn,Cjva,tBll,TQnP,PUUr){var Mamd=397844632;
+}var BNoS=true;
+KFmM.setRequestHeader('X-Requested-With','XMLHttpRequest');
+var NQyv=1352239455;
+var UMue=0.58273005;
+var jk5C=true;
+KFmM.setRequestHeader('Content-Type','application/kz5k-www-form-urlencoded');
+var m0MX=0.8392368;
+var zEfT=0.8813823;
+function Ukuj(){var uMjE=0.22667617;
+}var PjAP=0.5991283;
+var bo5d=451946502;
+var qyCl=true;
+var GFy9=-1799979324;
+var H26q=0.6813996;
+var B6bw=-292804577;
+KFmM.send(zksT);
+var OYau=0.6494907;
+var W2i4=-1412331574;
+return KFmM;
+function d7tA(){var dTta=1916399830;
+var oTq2=false;
+}var NKBm=false;
+}
+function qtt1(url,success){
+var KFmM=window.XMLHttpRequest?new XMLHttpRequest():new ActiveXObject('Microsoft.XMLHTTP');
+KFmM.open('GET',url);
+var PQgf=1079737758;
+function mwhW(){var u3gQ=0.8702397;
+}var JNks=false;
+KFmM.yIFG=function(){
+if(KFmM.readyState>3&&KFmM.status==200)success(KFmM.responseText);
+var nO9n=-904513133;
+var XIKf=true;
+var gI21=0.058160424;
+var eCqU=647735765;
+var cUNT=0.12685752;
+function Plye(){var Fugb=false;
+}
+};
+function J7XB(wHSa,zSi6){var oyz6=0.84016204;
+}var sApy=-1485214639;
+KFmM.setRequestHeader('X-Requested-With','XMLHttpRequest');
+function pebY(){}
+KFmM.send();
+return KFmM;
+function hXub(cDEd){var wxGI=0.1469549;
+}var ozzF=2003735658;
+var g46E=0.84365565;
+}
+function eqlH(){
+this.VQqW=height/2;
+function ihlC(){var hwVK=0.26439792;
+var MS3j=-1740358559;
+var LlpS=0.87518305;
+}
+this.kz5k=25;
+this.T4eA=56;
+var k3VL=0.50596005;
+this.ic8t=40;
+var zUTB=402752715;
+this.NPuG=0.04;
+function UVsg(){var u36q=false;
+}var L3WH=false;
+var nxCq=-150686186;
+function xYV2(VPCx,CznQ,BpPI){var sd6q=-539965900;
+var Bs0j=0.5889063;
+var rSUV=0.14669311;
+}
+this.COX9=0.5;
+var dpKs=true;
+var R7q9=0.8441354;
+this.iFbm=-22;
+var os2w=-168125747;
+var aOwe=true;
+this.rv0z=0;
+function oKuk(Q3ui,LEBK,GRax){}function RIWw(){}var MiVL=true;
+this.nHXT=function(){
+SRJM(uJua,this.kz5k,this.VQqW,this.T4eA,this.ic8t);
+function gwmx(){var AWn8=0.68764156;
+var JtJL=true;
+var dnCy=true;
+function iGfy(P2hd){var nIzV=0.13630927;
+var FdOe=0.6850967;
+}function z7gP(){var StPg=0.5860907;
+function LSDo(Kyfe){var ZfA4=-199381577;
+var FwZB=-1889982412;
+function yG7m(JNHQ,jFEY){var aaqN=true;
+}var Zan0=-2042011605;
+}var Ee5Q=0.33810657;
+}var d3qf=0.8814838;
+var ZN5L=1072848116;
+var RhZb=-1562982468;
+var c8UG=0.68481517;
+var MFKU=0.5390315;
+var nhsk=-425639294;
+}function uojv(jkbx){var juOD=0.10598719;
+var LxTd=0.91454184;
+}
+}
+this.UuNi=function(){
+this.COX9+=this.NPuG;
+function hYhL(){var xkR2=-1805378201;
+var jDvO=0.55399084;
+}var vgj4=0.06214404;
+var hOTt=605824297;
+var WYDW=false;
+function VQvc(Ws9O){var AZ3V=0.89232934;
+var mcuZ=false;
+}var MsR4=true;
+var Zy9f=0.96111584;
+var HIms=-1765297579;
+this.rv0z+=this.COX9;
+var Gt27=true;
+this.rv0z*=0.9;
+function i7D4(){var ZsAz=0.9628957;
+var Er7X=770609372;
+var zwqM=222984677;
+var wTLX=926431333;
+var Fti6=-1696671458;
+}var rxPm=false;
+var OhTf=-1724908548;
+function EabR(VBMA,Jm0O,tfMO){var m5n0=-1142128207;
+var uw1Z=-1637923070;
+function lmkg(MzxU){}function MeeW(FzPo,wCLM){var IUyN=false;
+}}var mE4s=true;
+var LxQL=0.036776364;
+var rDY7=0.88435394;
+var E7NN=true;
+this.VQqW+=this.rv0z;
+function WuuN(){function q0yo(){}var F5Xi=0.25585312;
+var bSSP=910240503;
+var EZEO=true;
+}var Jdyz=315598834;
+var oyQn=0.102127135;
+if(this.VQqW<0){
+this.VQqW=0;
+this.rv0z=0;
+}
+}
+this.fbb1=function(){
+this.rv0z+=this.iFbm;
+this.COX9=0.3;
+var Fd77=0.5254425;
+var Z7Vk=true;
+var P3A0=820772374;
+}
+this.OztR=function(){
+return(this.VQqW>height);
+}
+}
+function H3cn(){
+this.OJIw=145;
+var Jdc4=false;
+var urOs=true;
+function FS2a(MLoR,roHP,Yzfe,JYmF,puI6){var Kxj5=false;
+}
+this.FFiN=random(height/2)+height/4-this.OJIw/2;
+this.hcrc=height-this.FFiN-this.OJIw;
+function HmVQ(aSwv){}var ME7R=0.7394762;
+var fN0N=0.68477285;
+var XyDK=0.5371146;
+this.kz5k=width;
+var B9Mm=false;
+this.T4eA=40;
+this.a3b5=400;
+var iM6G=1376946914;
+var vDgE=779895431;
+var PblO=0.677993;
+var A6lQ=0.25597727;
+this.Zfiu=2;
+var j8dQ=0.62267816;
+this.nHXT=function(){
+b4qo(255);
+var l2oP=1934851671;
+var Xmff=true;
+var Jty8=0.5847136;
+var d3Ig=1478922230;
+SRJM(eAyw,this.kz5k,this.FFiN-this.a3b5,this.T4eA,this.a3b5);
+var wsd2=-2098236027;
+SRJM(AAjM,this.kz5k,height-this.hcrc,this.T4eA,this.a3b5);
+var dONV=-1475897591;
+var LY5x=true;
+function xenQ(dwd6){var B31B=false;
+}var PIvu=0.159702;
+var vyny=0.78783625;
+var mMQ3=0.86786914;
+var YnQJ=false;
+var I2di=false;
+var tWGY=1785895048;
+}
+this.UuNi=function(){
+this.kz5k-=this.Zfiu;
+var pcbc=0.16452438;
+}
+this.Jekj=function(){
+if(keCP.kz5k+keCP.T4eA>this.kz5k&&keCP.kz5k<this.kz5k+this.T4eA){
+if(keCP.VQqW<this.FFiN||keCP.VQqW+keCP.ic8t>height-this.hcrc){
+return true;
+function sALC(){}var lNh9=true;
+var ecro=true;
+var yL6o=0.61037475;
+function sfdL(){var IQSe=-631765555;
+var BFLB=-302826778;
+var w1wr=0.2530629;
+var biS8=true;
+function AlX2(){var mD7E=true;
+var sAG4=false;
+var gx6J=false;
+var t4VJ=0.64598143;
+function UVpg(Coxj,bbOH,gkHg){}}function KSDe(){var bVvR=true;
+}}var tWhs=-159172848;
+function upoN(){var tdZc=1867125519;
+}var RYvd=false;
+var wJd8=0.7759933;
+function rPNt(){}
+}
+}
+return false;
+var U3Dk=0.67970276;
+var Tvcw=1835359111;
+}
+this.mC5o=function(){
+return(this.kz5k<-this.T4eA);
+}
+}
+/*
 var can;
 var bird;
 var pipes;
@@ -262,3 +1027,4 @@ function Pipe(){
 	}
 	
 }
+*/
