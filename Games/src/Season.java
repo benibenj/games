1da5dcd6339d57a87f63bd986dcad11021f71019
Fabian Bösiger <boesiger.fabian@outlook.com>
2019-05-24 12:10:42 +0200

    Changes

--- Games/src/Season.java
+++ Games/src/Season.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import database.templates.BooleanTemplate;
 import database.templates.IntegerTemplate;
 import database.templates.ListTemplate;
 import database.templates.ObjectTemplate;
+import mailer.Mailer;
+import user.User;
 
 public class Season extends ObjectTemplate {
 	
@@ -33,7 +36,7 @@ public class Season extends ObjectTemplate {
 		this(0);
 	}
 	
-	public void update() {
+	public void update(Mailer mailer) {
 		if(duration.get() <= 0) {
 			if(!ended.get()) {
 				ended.set(true);
@@ -51,20 +54,37 @@ public class Season extends ObjectTemplate {
 					Player player = players.get(i);
 					ranking.add(player);
 					player.addRank(i);
+					
+					int reward = 0;
+					
 					switch(i) {
 					case 0:
 						player.addCoins(100);
+						reward = 100;
 						break;
 					case 1:
 						player.addCoins(50);
+						reward = 50;
 						break;
 					case 2:
 						player.addCoins(30);
+						reward = 30;
 						break;
 					default:
 						player.addCoins(10);
+						reward = 10;
 						break;
 					}
+					
+					User user = (User) database.load(User.class, player.getUsername());
+					HashMap <String, Object> variables = new HashMap <String, Object> ();
+					variables.put("username", user.getUsername());
+					variables.put("current-season", season.get());
+					variables.put("next-season", season.get() + 1);
+					variables.put("rank", i + 1);
+					variables.put("reward", reward);
+					mailer.send(user.getMail(), "{{print translate \"new-season-email-title\"}}", "season.html", user.getLanguages(), variables);
+					
 					player.resetFame();
 					database.update(player);
 				}
@@ -82,9 +102,9 @@ public class Season extends ObjectTemplate {
 		return season.get();
 	}
 
-	public void end() {
+	public void end(Mailer mailer) {
 		duration.set(0);
-		update();
+		update(mailer);
 	}
 
 	public int getDuration() {
